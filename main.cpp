e9dc22f382fa7993456f9283254c51ada7affbae
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 11:25:21 +0200

    ex4

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
-using namespace std;
+void replaceDotsWithExclamation(char* str) {
+    if (str != nullptr) {
+        for (int i = 0; str[i] != '\0'; ++i) {
+            if (str[i] == '.') {
+                str[i] = '!';
+            }
+        }
+    }
+}
 
-int main()
-{
-	
+int main() {
+    const int maxLength = 100; 
+    char input[maxLength];
+
+    std::cout << "Enter a string: ";
+    std::cin.getline(input, maxLength);
+
+    replaceDotsWithExclamation(input);
+
+    std::cout << "Result: " << input << std::endl;
+
+    return 0;
 }
