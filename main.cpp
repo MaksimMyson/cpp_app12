bbeeb252b9390c661a59ed4d4abfe389a8fa03e8
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 10:18:38 +0200

    ex1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-int main()
-{
-	
+int main() {
+    const int maxSize = 100; 
+
+    char inputString[maxSize];
+
+    cout << "Enter a string: ";
+    cin.getline(inputString, maxSize);
+
+    
+    for (int i = 0; inputString[i] != '\0'; ++i) {
+        if (inputString[i] == ' ') {
+            inputString[i] = '\t';
+        }
+    }
+
+   
+    cout << "Modified string: " << inputString << endl;
+
+    return 0;
 }
