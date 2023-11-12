a402803c1dfd90ea8d2e9f0ad167d506a22f90e7
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 11:02:36 +0200

    ex1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,40 @@
 
 using namespace std;
 
-int main()
-{
-	
+void removeCharacterAtIndex(char* str, int index) {
+    int length = 0;
+    while (str[length] != '\0') {
+        ++length;
+    }
+
+    if (index >= 0 && index < length) {
+        
+        for (int i = index; i < length; ++i) {
+            str[i] = str[i + 1];
+        }
+        str[length - 1] = '\0'; 
+    }
+    else {
+       
+        cout << "Invalid index." << endl;
+    }
+}
+
+int main() {
+    const int maxSize = 100; 
+
+    char input[maxSize];
+    int index;
+
+    cout << "Enter a string: ";
+    cin.getline(input, maxSize);
+
+    cout << "Enter the index of the character to remove: ";
+    cin >> index;
+
+    removeCharacterAtIndex(input, index);
+
+    cout << "Result: " << input << endl;
+
+    return 0;
 }
