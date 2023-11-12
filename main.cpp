549e4b0ec46bef7657d0ad68745fd820bba52c3c
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 11:35:35 +0200

    ex5

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
-using namespace std;
+int countOccurrences(const char* str, char target) {
+    int count = 0;
+    if (str != nullptr) {
+        for (int i = 0; str[i] != '\0'; i++) {
+            if (str[i] == target) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
 
-int main()
-{
-	
+int main() {
+    const int maxLength = 100;
+    char input[maxLength];
+    char target;
+
+    std::cout << "Enter a string: ";
+    std::cin.getline(input, maxLength);
+
+    std::cout << "Enter the target character to count: ";
+    std::cin >> target;
+
+    int occurrences = countOccurrences(input, target);
+
+    std::cout << "The character '" << target << "' occurs " << occurrences << " times in the string." << std::endl;
+
+    return 0;
 }
