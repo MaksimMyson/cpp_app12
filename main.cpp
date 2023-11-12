dbeb461b425ed2264ca91d71fd8a4c100a4bdd55
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 10:44:36 +0200

    ex3

--- main.cpp
+++ main.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
-int main()
-{
-	
+bool isAlpha(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+}
+
+int countWords(const char* sentence) {
+    int words = 0;
+    bool inWord = false;
+
+    for (int i = 0; sentence[i] != '\0'; ++i) {
+        if (isAlpha(sentence[i])) {
+            if (!inWord) {
+                words++;
+                inWord = true;
+            }
+        }
+        else {
+            inWord = false;
+        }
+    }
+
+    return words;
+}
+
+int main() {
+    char sentence[1000];
+
+    cout << "Enter a sentence: ";
+    cin.getline(sentence, sizeof(sentence));
+
+    int numberOfWords = countWords(sentence);
+
+    cout << "Number of words: " << numberOfWords << endl;
+
+    return 0;
 }
