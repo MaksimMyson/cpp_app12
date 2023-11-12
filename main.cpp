6832cc71cd4ed71889d8b48f87320b2365557f8b
MaksimMyson <maksimmyson33@gmail.com>
2023-11-12 10:38:34 +0200

    ex2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
-int main()
-{
-	
+int main() {
+    char str[100];
+    int letters = 0, digits = 0, others = 0;
+
+    cout << "Enter a string: ";
+    cin.getline(str, sizeof(str));
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (isalpha(str[i])) {
+            letters++;
+        }
+        else if (isdigit(str[i])) {
+            digits++;
+        }
+        else {
+            others++;
+        }
+    }
+
+    cout << "Letters: " << letters << endl;
+    cout << "Digits: " << digits << endl;
+    cout << "Other characters: " << others << endl;
+
+    return 0;
 }
